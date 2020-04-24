a3fa4c69eeca589d84d6fed80e6c153aefe9f447
“KimV-1086” <1086kimv@gmail.com>
2020-04-24 20:39:30 +0600

    test push

--- js/index.js
+++ js/index.js
@@ -9,32 +9,30 @@ $(document).ready(function(){
 
   const teamAva = $( ".team-ava" );
   const cardTeam = $('.team-our-title-links');
-
-
+  
   const avaAnimate = (target) => {
     
     teamAva.animate({
       width: "100px",
       height: "100px",
       opacity: 0.2
-    })
+    }, 0)
     
     target.animate({
       width: "163px",
       height: "163px",
       opacity: 1,
       }, 500);
-
-    cardTeam.collapse('hide').animate(600);
-
+     
   }
 
   
   teamAva.on('click', (e) => {
     
     const target = $(e.target);
+    cardTeam.collapse('hide');  
     avaAnimate(target);
-    
+
   });
 
   
